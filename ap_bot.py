424faf91908011bb222d5df7a412d8e2562c3899
HOKTOP <54412941+HOKTOP@users.noreply.github.com>
2023-01-06 12:56:09 +0200

    Create ap_bot.py

---
+++ ap_bot.py
@@ -0,0 +1,50 @@
+import requests
+import json
+from pytube import YouTube
+
+def geturlfacebook(url):
+    url2 = "https://facebook-reel-and-video-downloader.p.rapidapi.com/app/main.php"
+    querystring = {"url":f"{url}"}
+    headers = {
+	"X-RapidAPI-Key": "97f009c328msh0526e3854dee42bp14ec32jsnc22930cf6e50",
+	"X-RapidAPI-Host": "facebook-reel-and-video-downloader.p.rapidapi.com"
+    }
+    response = requests.request("GET", url2, headers=headers, params=querystring)
+    jsondata = json.loads(response.text)
+    #namevideo=jsondata['title']
+    hdvideurl = jsondata['links']['Download High Quality']
+    response.close()
+
+    return(hdvideurl)
+
+
+def urltube(url):
+     data=YouTube(url).streams.filter(progressive=True,file_extension='mp4').get_highest_resolution().url
+     return(data)
+
+def urlinsta(url):
+    url2 = "https://instagram-downloader-download-instagram-videos-stories.p.rapidapi.com/index"
+    querystring = {"url":f"{url}"}
+    headers = {
+	"X-RapidAPI-Key": "97f009c328msh0526e3854dee42bp14ec32jsnc22930cf6e50",
+	"X-RapidAPI-Host": "instagram-downloader-download-instagram-videos-stories.p.rapidapi.com"
+    }
+    response = requests.request("GET", url2, headers=headers, params=querystring)
+    jsondata=json.loads(response.text)
+    urla = jsondata['media'] 
+    response.close()
+
+    return(urla)
+def urltiktok(url):
+    url2 = "https://tiktok-video-no-watermark2.p.rapidapi.com/"
+    querystring = {"url":"https://www.tiktok.com/@tiktok/video/7106658991907802411","hd":"0"}
+    headers = {
+	"X-RapidAPI-Key": "97f009c328msh0526e3854dee42bp14ec32jsnc22930cf6e50",
+	"X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com"
+    }
+    response = requests.request("GET", url2, headers=headers, params=querystring)
+    jsondata=json.loads(response.text)
+    urla = jsondata['data']['play'] 
+    response.close()
+    return(urla)
+
